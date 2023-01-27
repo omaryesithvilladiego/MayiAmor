755ae7989214cb962fcd6b634c63d5517c461849
omaryesithvilladiego <123215875+omaryesithvilladiego@users.noreply.github.com>
2023-01-27 08:53:00 -0500

    Brenda Love

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import * as React from "react";
 import { motion } from "framer-motion";
 import "./App.css";
-import imagen_es from "./assets/mayi.png";
+import imagen_es from "./assets/WhatsApp Image 2023-01-27 at 8.41.47 AM.jpeg";
 
 function App() {
   const [hardcore, setHardcore] = useState(1);
@@ -13,10 +13,23 @@ function App() {
   const testiong = () => {
     setHardcore(Math.round(hardcore + Math.random() * 100));
     setishardcore(true)
-    if(hardcore>150) {
-      setmensaje("Oh my god, Mayi te ama muchoooooo!! ")
-    } else if(hardcore<150){
-      setmensaje("Oww no, Mayi no te ama mucho")
+    if(hardcore<50) {
+      setmensaje("Oh no, Brenda no te ama casi")
+    } else if(hardcore>50 && hardcore<100){
+      setmensaje("Ella te esta amando un poquito mas")
+    } else if(hardcore>100 && hardcore<150){
+      setmensaje("Ohh my god, Brenda te amaaaaa")
+    }else if(hardcore>150 && hardcore<200){
+      setmensaje("Ella te adora....")
+    }else if(hardcore>200 && hardcore<250){
+      setmensaje("Ahhhhhh mucho amor")
+    }else if(hardcore>250 && hardcore<300){
+      setmensaje("En el aire se respira amor por Montón")
+    }else if(hardcore>300 && hardcore<350){
+      setmensaje("Brenda está sintiendo mucho amor por ti")
+    }else{
+      setmensaje("Te informo que Brenda tiene novio, Ella no te puede amar más")
+      setHardcore(0)
     }
   };
   const showhard = () => {
@@ -34,11 +47,11 @@ function App() {
     <div className="App">
      
       <div>
-        <h1>Mayi's love calculator
+        <h1>Brenda Love
 </h1>
       </div>
      
-        <p>Presiona clic sobre la imagen para descubrir cuánto te ama Mayi</p>
+        <p>Presiona clic sobre la imagen para que Brenda te ame</p>
       
 
       <motion.button
@@ -50,10 +63,10 @@ function App() {
       > <img src={imagen_es} alt="" /></motion.button>
 
       <div id="backgrond" className={ishardcore ? "show":"none"}>
-        <p>Mayi te ama un {hardcore}%</p>
+        <p>Brenda te ama un {hardcore}%</p>
       </div>
 
-      <div className={ishardcore ? "show":"none"}>
+      <div id="mensaje" className={ishardcore ? "show":"none"}>
         <p className="mensaje">{mensaje}</p>
       </div>
 
